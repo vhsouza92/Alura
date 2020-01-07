9c40982760b37d7ef911700cb57e7a1de54dcd79
vhsouza <vhsouza92@gmail.com>
2020-01-07 11:50:05 -0300

    modulo nao finalizado

---
+++ Angular01/alurapic/src/app/photos/photo/photo.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { PhotoComponent } from './photo.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+@NgModule({ 
+    declarations: [
+        PhotoComponent,
+    ],
+    imports: [
+        CommonModule,
+        HttpClientModule
+    ],
+    exports: [ PhotoComponent ]
+})
+
+export class PhotoModule{}
